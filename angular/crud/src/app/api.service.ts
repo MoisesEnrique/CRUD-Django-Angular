aa1a13a782ed3558eb37400f9cc1d04a7f7232e3
MoisesEnrique <moises.mac@gmail.com>
2021-07-30 23:23:38 -0500

    AngularCrud - Creando l metodo para mostrar las peliculas desde el api de django, con una aplicacion json en angular

--- angular/crud/src/app/api.service.ts
+++ angular/crud/src/app/api.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
 
-  constructor() { }
+  baseurl = "http://127.0.0.1:8000";   /* la direccion base de django */
+
+  /* encabezados del html */
+  httpHeaders = new HttpHeaders({ 'Content-type': 'application/json'})
+
+  constructor(private http: HttpClient) { } /* habilitamos el modelo http en el lado del cliente */
+
+  getAllMovies() : Observable<any>{   /* Metodo para obtener las peliculas  */
+    return this.http.get(this.baseurl + '/movies/', {headers: this.httpHeaders});
+  }
 }
