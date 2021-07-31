5522993157336d0ca1c3a0322c31011a1c60a400
MoisesEnrique <moises.mac@gmail.com>
2021-07-31 00:18:33 -0500

    DjangoCrud - Añadiendo funcionalidad a settings.py

--- django/src/djangocrud/settings.py
+++ django/src/djangocrud/settings.py
@@ -28,6 +28,12 @@ DEBUG = True
 ALLOWED_HOSTS = []
 
 
+#Proporcionar acceso a la API
+CORS_ORIGIN_WHITELIST = [
+    'http://localhost:4200',
+]
+
+
 # Application definition
 
 INSTALLED_APPS = [
@@ -40,6 +46,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'api',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -50,6 +57,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
 ]
 
 ROOT_URLCONF = 'djangocrud.urls'
