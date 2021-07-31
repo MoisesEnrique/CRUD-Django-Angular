4798b4b9cf480e92a5e6a37df31dd9ac94ed408b
MoisesEnrique <moises.mac@gmail.com>
2021-07-30 21:22:08 -0500

    DjangoCrud - Creando el serializer

--- django/src/djangocrud/api/views.py
+++ django/src/djangocrud/api/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User, Group
+from rest_framework import viewsets
+from rest_framework import permissions
+from .serializers import MovieSerializer
 
-# Create your views here.
+
+class MovieViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = MovieSerializer
+
+
